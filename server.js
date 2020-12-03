92e85979154222481cf855ba29f65ee7ed863592
Gregor M <gregor.mlakar13@gmail.com>
2020-12-03 12:25:51 +0100

    added connection to DB

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const cors = require('cors');
+
+
+const connectDB = require('./config/db');
 
 const app = express(); 
 const port = process.env.PORT || 6000;
 
+//load env
+dotenv.config({path: './config/config.env'});
+
+//"body-parser"
 app.use(express.json({extened: true}));
+
+//cors
 app.use(cors());
 
+//connect to DB
+connectDB();
+
+//server RUN
 app.listen(port, error => {
     if(error) throw error;
     console.log('App listening on port' + ' ' + port);
